e9ce37c3ab61bc89de62eb351ba6305bf94f7732
Mengying Wang <mew897@g.harvard.edu>
2019-12-15 18:03:34 -0500

    test webhook

--- src/App.tsx
+++ src/App.tsx
@@ -18,7 +18,7 @@ const App: React.FC = () => {
           </a> Final Project App!
         </code>
         <p>
-          Team: James Fulford, {/* TODO(teammates): Put your name here! */}
+          Team: James Fulford, Nora, Mengying {/* TODO(teammates): Put your name here! */}
         </p>
       </header>
     </div>
