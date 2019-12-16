15094c97c55f133bc5fab2ed2ad388c33f488fb3
James Fulford <james.fulford@outlook.com>
2019-12-16 10:53:43 -0500

    fix(build): Error handling

--- _build.sh
+++ _build.sh
@@ -1,4 +1,26 @@
 #!/bin/bash
+echo ">> Installing dependencies..."
 yarn --frozen-lockfile --production
+install_code=$?
+if [[ "0" != "$install_code" ]]; then
+    echo "<< Install failed!"
+    exit $install_code
+fi
+echo ">> Installing dependencies... done"
+echo ">> Running tests..."
 CI=true yarn test
+test_code=$?
+if [[ "0" != "$test_code" ]]; then
+    echo "<< Tests failed!"
+    exit $test_code
+fi
+echo ">> Running tests... done"
+
+echo ">> Building assets..."
 yarn build
+build_code=$?
+if [[ "0" != "$build_code" ]]; then
+    echo "<< Build failed!"
+    exit $build_code
+fi
+echo ">> Building assets... done"
--- build.sh
+++ build.sh
@@ -3,12 +3,12 @@
 # That directory should be served statically.
 # Entry point is index.html.
 
-echo ">>> Building bundle..."
+echo ">>> Building app..."
 docker run --rm -i -v "$PWD:/build" -w /build node:10.16.0-slim "./_build.sh"
 build_error_code=$?
 if [[ "0" != "$build_error_code" ]]; then
-    echo "<<< Build failed!"
+    echo "<<< App Build failed!"
     exit $build_error_code
 fi
-echo ">>> Building bundle... done"
+echo ">>> Building app... done"
 echo
